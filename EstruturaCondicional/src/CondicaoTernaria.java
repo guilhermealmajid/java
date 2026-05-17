f223dd55b6e666b8c0aec647dbb477a4c828d514
guilhermealmajid <guilhermealmajid@gmail.com>
2026-05-17 20:56:40 -0300

    up

---
+++ EstruturaCondicional/src/CondicaoTernaria.java
@@ -0,0 +1,19 @@
+void main() {	
+
+	double preco = 34.5;
+//	double desconto;
+//
+//	if (preco < 20.0) {
+//		desconto = preco * 0.1;
+//	} else {
+//		desconto = preco * 0.05;
+//	}
+	
+	double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+	//Statement: (condição) ? valor_se_verdadeiro : valor_se_falso.
+	//Feito para ser uma expressão e nao estrutura de controle de fluxo completa. Ser mais elegante e simples.
+	
+	System.out.println("Desconto: " + desconto);
+	
+	
+}
