db1246d232cd6ec7a27fbca6884d9a220763ca39
guilhermealmajid <guilhermealmajid@gmail.com>
2026-05-18 19:28:41 -0300

    up

--- EstruturaCondicional/src/Escopo_e_Inicializacao.java
+++ EstruturaCondicional/src/Escopo_e_Inicializacao.java
@@ -5,7 +5,7 @@ void main() {
 	/* Variável nao inicializada o que gera erro de compilação. 
 	 * Iniciar com valor 0.0 ou na Estrutura de controle no else colocar a variavel com valor.
 	 * Obs.: Variáveis globais sao usadas em toda a Classe e em qualquer método ou bloco de código como Estrutura de controle,
-	 * o inverso disso ela só pode ser usada onde foi declarada. 
+	 * o inverso disso ela só pode ser usada onde foi declarada. .
 	 */
 	
 	if (price < 200.0) {
