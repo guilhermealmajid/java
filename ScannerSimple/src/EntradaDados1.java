68d7b1d59461eeea448a0941da8715c5d5b14930
Guilherme Cunha de Almeida <guilhermealmajid@gmail.com>
2026-08-17 17:21:43 -0300

    alteracao

--- ScannerSimple/src/EntradaDados1.java
+++ ScannerSimple/src/EntradaDados1.java
@@ -26,6 +26,7 @@ public class EntradaDados1 {
     System.out.println("Digite um caractere:");
     z = sc.next().charAt(0); // Ler um char, usando next com detalhe para index 0 ou seja primeiro caractere
                              // da string.
+    // alteracao
     System.out.println("Você digitou: " + z);
 
     System.out.println("Digitar Nome, inteiro e Double: "); // Ler dados numa mesma linha separados por espaço, tipo
